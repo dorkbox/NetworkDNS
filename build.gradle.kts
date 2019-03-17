1cb751429f285451129800d332ce613e1ce4acbe
nathan <nathan@dorkbox.com>
2019-03-17 21:12:53 +0100

    Code cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,10 +54,10 @@ object Extras {
     const val version = "1.0"
 
     // set as project.ext
-    const val name = "Network DNS"
+    const val name = "NetworkDNS"
     const val id = "NetworkDNS"
     const val vendor = "Dorkbox LLC"
-    const val url = "https://git.dorkbox.com/dorkbox/Network-DNS"
+    const val url = "https://git.dorkbox.com/dorkbox/NetworkDNS"
     val buildDate = Instant.now().toString()
 
     val JAVA_VERSION = JavaVersion.VERSION_1_8.toString()
@@ -100,120 +100,23 @@ if (propsFile.canRead()) {
 
 
 licensing {
-//    license(License.APACHE_2) {
-//        author(Extras.vendor)
-//        url(Extras.url)
-//        note(Extras.description)
-//    }
-//
-//    license("Dorkbox Utils", License.APACHE_2) {
-//        author(Extras.vendor)
-//        url("https://git.dorkbox.com/dorkbox/Utilities")
-//    }
-//
-//    license("Bennidi Iterator", License.MIT) {
-//        copyright(2012)
-//        author("Benjamin Diedrichsen")
-//        url("https://github.com/bennidi/mbassador")
-//        note("Fast iterators from the MBassador project")
-//    }
-//
-//    license("BouncyCastle", License.MIT) {
-//        copyright(2009)
-//        author("The Legion Of The Bouncy Castle")
-//        url("http://www.bouncycastle.org")
-//    }
-//
-//    license("ObjectPool", License.APACHE_2) {
-//        author("dorkbox, llc")
-//        url("https://git.dorkbox.com/dorkbox/ObjectPool")
-//    }
+    license(License.APACHE_2) {
+        author(Extras.vendor)
+        url(Extras.url)
+        note(Extras.description)
+    }
 
-//    license("FastThreadLocal", License.BSD_3) {
-//        copyright(2014)
-//        author("Lightweight Java Game Library Project")
-//        author("Riven")
-//        url("https://github.com/LWJGL/lwjgl3/blob/5819c9123222f6ce51f208e022cb907091dd8023/modules/core/src/main/java/org/lwjgl/system/FastThreadLocal.java")
-//    }
-//
-//    license("Javassist", License.BSD_3) {
-//        copyright(1999)
-//        author("Shigeru Chiba")
-//        author("Bill Burke")
-//        author("Jason T. Greene")
-//        url("http://www.csg.is.titech.ac.jp/~chiba/java")
-//        note("Licensed under the MPL/LGPL/Apache triple license")
-//    }
-//
-//    license("Kryo", License.BSD_3) {
-//        copyright(2008)
-//        author("Nathan Sweet")
-//        url("https://github.com/EsotericSoftware/kryo")
-//    }
-//
-//    license("kryo-serializers", License.APACHE_2) {
-//        copyright(2010)
-//        author("Martin Grotzke")
-//        author("Rafael Winterhalter")
-//        url("https://github.com/magro/kryo-serializers")
-//    }
-//
-//    license("KryoNet RMI", License.BSD_3) {
-//        copyright(2008)
-//        author("Nathan Sweet")
-//        url("https://github.com/EsotericSoftware/kryonet")
-//    }
-//
-//    license("LAN HostDiscovery from Apache Commons JCS", License.APACHE_2) {
-//        copyright(2014)
-//        author("The Apache Software Foundation")
-//        url("https://issues.apache.org/jira/browse/JCS-40")
-//    }
-//
-//    license("LZ4 and XXhash", License.APACHE_2) {
-//        copyright(2011)
-//        copyright(2012)
-//        author("Yann Collet")
-//        author("Adrien Grand")
-//        url("https://github.com/jpountz/lz4-java")
-//    }
+    license("ObjectPool", License.APACHE_2) {
+        author("dorkbox, llc")
+        url("https://git.dorkbox.com/dorkbox/ObjectPool")
+    }
 
-//    license("MathUtils, IntArray, IntMap", License.APACHE_2) {
-//        copyright(2013)
-//        author("Mario Zechner <badlogicgames@gmail.com>")
-//        author("Nathan Sweet <nathan.sweet@gmail.com>")
-//        url("http://github.com/libgdx/libgdx/")
-//    }
-//
-//    license("MinLog-SLF4J", License.APACHE_2) {
-//        copyright(2008)
-//        author("dorkbox, llc")
-//        author("Nathan Sweet")
-//        author("Dan Brown")
-//        url("https://git.dorkbox.com/dorkbox/MinLog-SLF4J")
-//        url("https://github.com/EsotericSoftware/minlog")
-//        note("Drop-in replacement for MinLog to log through SLF4j.")
-//    }
-//
-//    license("ReflectASM", License.BSD_3) {
-//        copyright(2008)
-//        author("Nathan Sweet")
-//        url("https://github.com/EsotericSoftware/reflectasm")
-//    }
-//
-//    license("SLF4J", License.MIT) {
-//        copyright(2008)
-//        author("QOS.ch")
-//        url("http://www.slf4j.org")
-//    }
-//
-//    license("TypeTools", License.APACHE_2) {
-//        copyright(2017)
-//        author("Jonathan Halterman")
-//        url("https://github.com/jhalterman/typetools/")
-//        note("Tools for resolving generic types")
-//    }
-//
+    license("Kryo", License.BSD_3) {
+        copyright(2008)
+        author("Nathan Sweet")
+        url("https://github.com/EsotericSoftware/kryo")
+    }
+
     license("XBill DNS", License.BSD_3) {
         copyright (2005)
         author ("Brian Wellington")
@@ -243,6 +146,7 @@ sourceSets {
 }
 
 repositories {
+    mavenLocal() // this must be first!
     jcenter()
 }
 
@@ -265,25 +169,6 @@ tasks.compileJava.get().apply {
     println("\tCompiling classes to Java $sourceCompatibility")
 }
 
-
-dependencies {
-    val netty = api("io.netty:netty-all:4.1.32.Final")
-    val kryo = api("com.esotericsoftware:kryo:4.0.2")
-    api("net.jpountz.lz4:lz4:1.3.0")
-
-    api("com.dorkbox:Network:2.17")
-    api("com.dorkbox:ObjectPool:2.11")
-
-    val slf4j = implementation ("org.slf4j:slf4j-api:1.7.25")
-
-    testCompile("junit:junit:4.12")
-    testCompile("ch.qos.logback:logback-classic:1.2.3")
-}
-
-
-///////////////////////////////
-//////    Jar Tasks
-///////////////////////////////
 tasks.jar.get().apply {
     manifest {
         // https://docs.oracle.com/javase/tutorial/deployment/jar/packageman.html
@@ -297,6 +182,17 @@ tasks.jar.get().apply {
         attributes["Implementation-Version"] = Extras.buildDate
         attributes["Implementation-Vendor"] = Extras.vendor
     }
+}
+
+dependencies {
+    api("com.dorkbox:Network:2.17")
+    api("io.netty:netty-all:4.1.32.Final")
+    api("com.esotericsoftware:kryo:4.0.2")
+
+    implementation ("org.slf4j:slf4j-api:1.7.25")
+
+    testCompile("junit:junit:4.12")
+    testCompile("ch.qos.logback:logback-classic:1.2.3")
 }
 
 

20ac888d965a005db7d4a447c20d93893e59725f
Robinson <robinson@dorkbox.com>
2021-05-02 22:42:20 +0200

    JPMS support

--- src9/module-info.java
+++ src9/module-info.java
@@ -1,5 +1,13 @@
 module dorkbox.dns {
+    exports dorkbox.dns;
+    exports dorkbox.dns.dns.constants;
+    exports dorkbox.dns.dns.exceptions;
+    exports dorkbox.dns.dns.records;
+
     requires dorkbox.netutil;
     requires dorkbox.utilities;
     requires dorkbox.updates;
+
+    requires io.netty.all;
+    requires org.slf4j;
 }
